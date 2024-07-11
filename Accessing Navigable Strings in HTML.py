a47d09dccc6c010837cf945f42329f74d3f5008c
Shubham Singh <32100935+ShubhamSinghCMR@users.noreply.github.com>
2024-07-12 00:15:19 +0530

    Accessing Navigable Strings in HTML

---
+++ Accessing Navigable Strings in HTML.py
@@ -0,0 +1,15 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = "https://webscraper.io/test-sites/e-commerce/allinone/computers"
+
+r=requests.get(url)
+
+if r.status_code != 200:
+    print("Unable to access website")
+
+soup = BeautifulSoup(r.text, "lxml")
+
+tag = soup.div.p
+
+print (tag.string)
